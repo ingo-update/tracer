24df55d2a3d751c1abdf82872370deedd2f62931
Ingemar Åberg <ingo@update.uu.se>
2021-08-13 15:42:08 +0200

    Use macros instead of get functions in options module

--- src/options.h
+++ src/options.h
@@ -25,20 +25,20 @@ struct options_t
   shading_mode shading;
 };
 
-options options_get_options(int argc, char **argv);
+#define options_get_pict_pix_width(opt)   (opt->pict_pix_width)
+#define options_get_pict_pix_height(opt)  (opt->pict_pix_height)
+#define options_get_pict_real_width(opt)  (opt->pict_real_width)
+#define options_get_pict_real_height(opt) (opt->pict_real_height)
+#define options_get_zoom(opt)             (opt->zoom)
+#define options_get_reflection_depth(opt) (opt->reflection_depth)
+#define options_get_rx(opt)               (opt->rx)
+#define options_get_ry(opt)               (opt->ry)
+#define options_get_in_file_name(opt)     (opt->in_file_name)
+#define options_get_out_file_name(opt)    (opt->out_file_name)
+#define options_get_comment(opt)          (opt->comment)
+#define options_get_shading(opt)          (opt->shading)
+#define options_get_ppm_mode(opt)         (opt->ppm_mode)
 
-int options_get_pict_pix_width(options opt);
-int options_get_pict_pix_height(options opt);
-real options_get_pict_real_width(options opt);
-real options_get_pict_real_height(options opt);
-real options_get_zoom(options opt);
-int options_get_reflection_depth(options opt);
-real options_get_rx(options opt);
-real options_get_ry(options opt);
-char *options_get_in_file_name(options opt);
-char *options_get_out_file_name(options opt);
-char *options_get_comment(options opt);
-shading_mode options_get_shading(options opt);
-ppmtype options_get_ppm_mode(options opt);
+options options_get_options(int argc, char **argv);
 
 #endif /* _OPTIONS_H_ */
